3ec4297aee4fa65a546030f55164210a1566ba07
kangdonguk <kangdongouk@gmail.com>
2025-03-08 17:48:58 +0900

    #6.10 Hero

--- lib/widgets/webtoon_widget.dart
+++ lib/widgets/webtoon_widget.dart
@@ -26,22 +26,25 @@ class Webtoon extends StatelessWidget {
       },
       child: Column(
         children: [
-          Container(
-            width: 250,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              boxShadow: [
-                BoxShadow(
-                  blurRadius: 15,
-                  offset: Offset(10, 10),
-                  color: Colors.black.withValues(alpha: 0.3),
-                ),
-              ],
-            ),
-            clipBehavior: Clip.hardEdge,
-            child: Image.network(
-              thumb,
-              headers: const {"Referer": "https://comic.naver.com"},
+          Hero(
+            tag: id,
+            child: Container(
+              width: 250,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 15,
+                    offset: Offset(10, 10),
+                    color: Colors.black.withValues(alpha: 0.3),
+                  ),
+                ],
+              ),
+              clipBehavior: Clip.hardEdge,
+              child: Image.network(
+                thumb,
+                headers: const {"Referer": "https://comic.naver.com"},
+              ),
             ),
           ),
           SizedBox(height: 10),
